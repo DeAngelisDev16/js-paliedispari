6a63e0695579feea2e30420ea963218fd144d8c7
Shelbot77 <alessiodeangelis021@gmail.com>
2022-11-29 15:29:17 +0100

    Tentativo impostazione funzione

---
+++ myscript.js
@@ -0,0 +1,39 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola:
+Creare una funzione per capire se la parola inserita è palindroma
+
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer attraverso una funzione.
+Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari attraverso un'altra funzione
+Dichiariamo chi ha vinto.
+
+Consigli del giorno
+Scriviamo sempre in italiano i passaggi che vogliamo fare
+Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti. */
+
+//prova funzionamento funzione;
+function sommaNumeri (num1, num2){
+    const somma = num1 + num2 ;
+    return somma;
+} //=====> nb: Tutto ciò che sta dopo return non viene eseguito;
+
+//console.log(sommaNumeri (2, 4));
+
+//NB: TUTTE LE VARIABILI UTILIZZATE ALL'INTERNO DI UNA FUNZIONE DEVONO ESSERE STATE DICHIARATE DENTRO ALLA FUNZIONE (IMPORTANTE)!!
+
+function palindromeTrueOrNot (word){
+    let userWord = prompt('Inserisci una parola');
+
+}
+
+/* function palindrome(str) {
+    return true;
+  }
+  console.log(palindrome("eye")); */
+    
+
+
+
+
+
